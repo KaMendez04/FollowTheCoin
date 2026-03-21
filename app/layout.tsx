c7bc7ba12d3ffcc77b666a0d988cfde7e12562c1
KaMendez04 <katherynmq@gmail.com>
2026-03-21 15:22:53 -0600

    nombre pagina

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Pixel Friends',
+  title: 'Follow The Coin',
   description: 'Interactive pixel art characters greeting you!',
   generator: 'v0.app',
   icons: {
